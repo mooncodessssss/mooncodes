1531af66c2140301ca0ca9a73b3653a5be06adbf
vampiregirl0803 <sunnymahala63@gmail.com>
2025-01-27 14:32:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MY-MOONREPO/moon",
+    "https://github.com/vampiregirl0803/moond",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
