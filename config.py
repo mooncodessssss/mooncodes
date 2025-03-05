d3acffd834c2f49c96da15de6453e1ee4a695980
mooncodessssss <digitalocean642@gmail.com>
2025-03-05 21:54:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,13 +21,9 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
 # Chat id of a group for logging bot's activities
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID","-1002222604638"))
 
-# Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://yt.okflix.top/api') ## E.G https://yt.okflix.top/api/jADTdg-o8i0 Returns 
-
 # Get this value from @MissRose_Bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID","6391774843"))
 
-## Fill these variables if you're deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME","moonmusics")
 # Get it from http://dashboard.heroku.com/account
